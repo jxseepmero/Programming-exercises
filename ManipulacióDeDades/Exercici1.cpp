7181edcb4a0bae321652ca45bfef7173d8e39a5a
jxseepmero <josep.antoni@enti.cat>
2025-02-24 19:05:40 +0100

    Create Exercici1.cpp

---
+++ ManipulacióDeDades/Exercici1.cpp
@@ -0,0 +1,9 @@
+/*==========INTRODUCCIÓ A LA PROGRAMACIÓ=========*/
+
+#include <iostream> // biblioteca estàndar de C++ que proporciona funcionalitats d'entrada i sortida.
+
+int main() 
+{
+	std::cout << "Hello World!!\n"; // Imprimeixo el missatge en la consola.
+	return 0;
+}
