e2d8fbf417137aeea300e920c0b5df16f8ff8ffd
jxseepmero <josep.antoni@enti.cat>
2025-02-24 19:12:44 +0100

    Create Ejercicio2.cpp

---
+++ ManipulaciónDeDatos/Ejercicio2.cpp
@@ -0,0 +1,12 @@
+/*==========INTRODUCCIÓ A LA PROGRAMACIÓ=========*/
+
+// 2. Obtén el siguiente resultado: "Texto1 Texto2" por pantalla usando las secuencias de escape.
+
+
+#include <iostream> // biblioteca estàndar de C++ que proporciona funcionalitats d'entrada i sortida.
+
+int main() 
+{
+	std::cout << "Texto1" << " " << "Texto2\n"; // Añado entre texto y texto un espacio para conseguir el resultado deseado.
+	return 0;
+}
