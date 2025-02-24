fdc80fbf50481a3ed869c72421358e0b350b74ce
jxseepmero <josep.antoni@enti.cat>
2025-02-24 19:07:33 +0100

    Update and rename Exercici1.cpp to Ejercicio1.cpp

---
+++ ManipulaciónDeDatos/Ejercicio1.cpp
@@ -0,0 +1,11 @@
+/*==========INTRODUCCIÓ A LA PROGRAMACIÓ=========*/
+
+// 1. Imprime el mensaje de “Hello World” por pantalla.
+
+#include <iostream> // biblioteca estándar de C++ que proporciona funcionalidades de entrada i salida.
+
+int main() 
+{
+	std::cout << "Hello World!!\n"; // Imprimo el mensaje en la consola.
+	return 0;
+}
